85b616629f47d6c3ff1d17fe08e66b49d8aec4f6
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-05-21 20:32:42 +0700

    role

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name'      => 'Admin',
+            'email'     => 'admin@gmail.com',
+            'password'  => Hash::make('admin12345')
+        ]);
+        $user = User::create([
+            'name'      => 'User',
+            'email'     => 'user@gmail.com',
+            'password'  => Hash::make('user12345')
+        ]);
+        User::factory(50)->create();
+    }
+}
