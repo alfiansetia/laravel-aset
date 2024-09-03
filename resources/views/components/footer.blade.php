a383d0d7b92c8a078e35d361ad5b9791c2d31172
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-09-03 13:30:12 +0700

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2018 <div class="bullet"></div>
+        Copyright &copy; {{ date('Y') }} <div class="bullet"></div>
         {{-- Design By <a href="https://nauv.al/">Muhamad Nauval Azhar</a> --}}
     </div>
     <div class="footer-right">
